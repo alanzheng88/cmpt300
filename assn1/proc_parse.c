bbdd1d29ac81f1b443ae9cda749d7fbee5d6e811
Alan Zheng <alanz@sfu.ca>
2017-01-23 03:58:25 -0800

    display all required information

--- assn1/proc_parse.c
+++ assn1/proc_parse.c
@@ -15,9 +15,6 @@
 #define UPTIME_FILE  "/proc/uptime"
 #define BUFFER_SIZE  1024
 
-char* getProcessorType() {
-
-}
 
 int openFile(char* filename, FILE** file) {
   if ( (*file = fopen(filename, "r")) == NULL ) {
@@ -25,35 +22,35 @@ int openFile(char* filename, FILE** file) {
             filename, strerror(errno));
     return -1;
   }
-  printf("file successfully opened: %s\n", filename);
   return 0;
 }
 
-void parse(FILE* file, char* result, char* search) {
+void parse(FILE* file, char** result, char* search, char* delimiter) {
   char* buffer = (char *) malloc(BUFFER_SIZE);
   char* status = (char *) malloc(sizeof(NULL));
   char* currentToken = (char *) malloc(BUFFER_SIZE);
-  
+ 
   do {
     status = fgets(buffer, BUFFER_SIZE, file); 
       
       // printf("DEBUG: %s\n", buffer);
     
     // !*search equivalent to (search[0] != '\0')
-    if (search && !*search) {
+    if ( !search || (search && !*search) ) {
       // search is empty
+      strcpy(*result, buffer);
       continue;
     }
 
     if (strstr(buffer, search)) {
-      currentToken = strtok(buffer, ":");
+      currentToken = strtok(buffer, delimiter);
       while (currentToken != NULL) {
         
           // printf("DEBUG: FOUND - %s\n", currentToken);
 
         currentToken = strtok(NULL, ":");
         if (currentToken != NULL) {
-          strcpy(result, currentToken);
+          strcpy(*result, currentToken);
         }
       }
       status = NULL; 
@@ -66,19 +63,50 @@ void parse(FILE* file, char* result, char* search) {
   free(currentToken);
 }
 
-int main() {
 
+char* getSystemInfo(char* filename, char* filter, char* delimiter) {
   FILE* file;
-  int statusCode;
-
-  statusCode = openFile(CPUINFO_FILE, &file);
-  if (statusCode == -1) { return -1; }
-
-
+  int statusCode = openFile(filename, &file);
+  if (statusCode == -1) { return NULL; }
   char* result = (char *) malloc(BUFFER_SIZE);
-  parse(file, result, "model name");
-  printf("result is: %s\n", result);
-  free(result);
+  parse(file, &result, filter, delimiter);
+  //printf("result is: %s\n", result);
+  return result;
+}
+
+void printProcessorType() {
+  char* cpuInfo = getSystemInfo(CPUINFO_FILE, "model name", ":");
+  if (!cpuInfo) { printf("invalid cpu info file provided"); }
+  printf("CPU Model Name: %s", cpuInfo);
+  free(cpuInfo);
+}
+
+void printKernelVersion() {
+  char* versionInfo = getSystemInfo(VERSION_FILE, NULL, NULL);
+  if (!versionInfo) { printf("invalid version file provided"); }
+  printf("Kernel Version: %s", versionInfo);
+  free(versionInfo);
+}
+
+void printTotalMemoryAmount() {
+  char* memoryInfo = getSystemInfo(MEMINFO_FILE, "MemTotal", ":");
+  if (!memoryInfo) { printf("invalid meminfo file provided"); }
+  printf("Main Memory Total: %s", memoryInfo);
+  free(memoryInfo);
+}
+
+void printTimeSinceSystemLastBooted() {
+  char* uptimeInfo = getSystemInfo(UPTIME_FILE, " ", " ");
+  if (!uptimeInfo) { printf("invalid uptime file provided"); }
+  printf("Uptime: %s", uptimeInfo);
+  free(uptimeInfo);
+}
+
+int main() {
+  printProcessorType();
+  printKernelVersion();
+  printTotalMemoryAmount();
+  printTimeSinceSystemLastBooted();
 
   return 0;
 }
