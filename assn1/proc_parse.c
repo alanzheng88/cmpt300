268e0ada1fb9764980405038c960f28f9363efc2
Alan Zheng <alanz@sfu.ca>
2017-01-23 18:39:08 -0800

    format output

--- assn1/proc_parse.c
+++ assn1/proc_parse.c
@@ -6,14 +6,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <errno.h>
 #include <unistd.h>
 
-#define CPUINFO_FILE "/proc/cpuinfo"
-#define VERSION_FILE "/proc/version"
-#define MEMINFO_FILE "/proc/meminfo"
-#define UPTIME_FILE  "/proc/uptime"
-#define BUFFER_SIZE  1024
+#define CPUINFO_FILE         "/proc/cpuinfo"
+#define VERSION_FILE         "/proc/version"
+#define MEMINFO_FILE         "/proc/meminfo"
+#define UPTIME_FILE          "/proc/uptime"
+#define BUFFER_SIZE          1024
+#define COLUMN_PRINT_PADDING -18
 
 
 int openFile(char* filename, FILE** file) {
@@ -71,6 +73,42 @@ void parse(FILE* file, char** result, char* search, char* delimiter) {
   free(currentToken);
 }
 
+char* trim(char* str) {
+  // source: http://stackoverflow.com/questions/122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
+
+  size_t len = 0;
+  char *frontp = str;
+  char *endp = NULL;
+
+  if (str == NULL) { return NULL; }
+  if (*str == '\0') { return str; }
+
+  len = strlen(str);
+  endp = str + len;
+
+  while (isspace(*frontp)) { frontp++; }
+  if (endp != frontp) {
+    while ( (isspace(*endp)) && (endp != frontp) ) { endp--; }
+    // endp is not pointing to the last character
+  }
+
+  // there is whitespace at the end so need to change location of \0
+  if (str + len - 1 != endp) {
+    *(endp + 1) = '\0';
+  }
+
+  // reusing pointer to point at front of the string buffer
+  endp = str;
+  if (frontp != str) {
+    while (*frontp) {
+      *endp = *frontp;
+      endp++;
+      frontp++;
+    }
+    *endp = '\0';
+  }
+  return str;
+}
 
 char* getSystemInfo(char* filename, char* filter, char* delimiter) {
   FILE* file;
@@ -78,42 +116,44 @@ char* getSystemInfo(char* filename, char* filter, char* delimiter) {
   if (statusCode == -1) { return NULL; }
   char* result = (char *) malloc(BUFFER_SIZE);
   parse(file, &result, filter, delimiter);
-  return result;
+  return trim(result);
 }
 
 void printProcessorType() {
   char* cpuInfo = getSystemInfo(CPUINFO_FILE, "model name", ":");
   if (!cpuInfo) { printf("invalid cpu info file provided"); }
-  printf("CPU Model Name: %s", cpuInfo);
+  printf("%*s: %s", COLUMN_PRINT_PADDING, "CPU Model Name",  cpuInfo);
   free(cpuInfo);
 }
 
 void printKernelVersion() {
   char* versionInfo = getSystemInfo(VERSION_FILE, NULL, NULL);
   if (!versionInfo) { printf("invalid version file provided"); }
-  printf("Kernel Version: %s", versionInfo);
+  printf("%*s: %s", COLUMN_PRINT_PADDING, "Kernel Version", versionInfo);
   free(versionInfo);
 }
 
 void printTotalMemoryAmount() {
   char* memoryInfo = getSystemInfo(MEMINFO_FILE, "MemTotal", ":");
   if (!memoryInfo) { printf("invalid meminfo file provided"); }
-  printf("Main Memory Total: %s", memoryInfo);
+  printf("%*s: %s", COLUMN_PRINT_PADDING, "Main Memory Total", memoryInfo);
   free(memoryInfo);
 }
 
 void printTimeSinceSystemLastBooted() {
   char* uptimeInfo = getSystemInfo(UPTIME_FILE, "", NULL);
   if (!uptimeInfo) { printf("invalid uptime file provided"); }
-  printf("Uptime: %s\n", uptimeInfo);
+  printf("%*s: %s Seconds", COLUMN_PRINT_PADDING, "Uptime", uptimeInfo);
   free(uptimeInfo);
 }
 
 int main() {
+  printf("\n");
   printProcessorType();
   printKernelVersion();
   printTotalMemoryAmount();
   printTimeSinceSystemLastBooted();
+  printf("\n\n");
 
   return 0;
 }
