a4f52c372f09814f4045a14cb9ea75cf5cec3e25
Alan Zheng <alanz@sfu.ca>
2017-01-23 19:03:40 -0800

    fix error formatting

--- assn1/proc_parse.c
+++ assn1/proc_parse.c
@@ -121,28 +121,28 @@ char* getSystemInfo(char* filename, char* filter, char* delimiter) {
 
 void printProcessorType() {
   char* cpuInfo = getSystemInfo(CPUINFO_FILE, "model name", ":");
-  if (!cpuInfo) { printf("\ninvalid cpu info file provided"); }
+  if (!cpuInfo) { printf("\ninvalid cpu info file provided"); return; }
   printf("%*s: %s", COLUMN_PRINT_PADDING, "CPU Model Name",  cpuInfo);
   free(cpuInfo);
 }
 
 void printKernelVersion() {
   char* versionInfo = getSystemInfo(VERSION_FILE, NULL, NULL);
-  if (!versionInfo) { printf("\ninvalid version file provided"); }
+  if (!versionInfo) { printf("\ninvalid version file provided"); return; }
   printf("%*s: %s", COLUMN_PRINT_PADDING, "Kernel Version", versionInfo);
   free(versionInfo);
 }
 
 void printTotalMemoryAmount() {
   char* memoryInfo = getSystemInfo(MEMINFO_FILE, "MemTotal", ":");
-  if (!memoryInfo) { printf("\ninvalid meminfo file provided"); }
+  if (!memoryInfo) { printf("\ninvalid meminfo file provided"); return; }
   printf("%*s: %s", COLUMN_PRINT_PADDING, "Main Memory Total", memoryInfo);
   free(memoryInfo);
 }
 
 void printTimeSinceSystemLastBooted() {
   char* uptimeInfo = getSystemInfo(UPTIME_FILE, "", NULL);
-  if (!uptimeInfo) { printf("\ninvalid uptime file provided"); }
+  if (!uptimeInfo) { printf("\ninvalid uptime file provided"); return; }
   printf("%*s: %s Seconds", COLUMN_PRINT_PADDING, "Uptime", uptimeInfo);
   free(uptimeInfo);
 }
